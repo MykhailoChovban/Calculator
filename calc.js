fa7bcfc1b7eafe673fc8500867e87e0a756d08ce
Mykhailo Chovban <mykhailo.chovban@gmail.com>
2022-09-17 11:50:19 +0300

    own designe

---
+++ calc.js
@@ -0,0 +1,10 @@
+function insert(num) {
+  if (document.getElementById('res').value == '0') {
+    document.getElementById('res').value = num;
+  }
+ else {
+    document.getElementById('res').value += num;
+    console.log(num); 
+  }
+}
+
